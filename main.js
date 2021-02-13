c0eb1233c8c73dd1d0df5547f19553d4e56bfad9
Rahat <djrayhan8@gmail.com>
2021-02-13 21:37:39 +0600

    Done with issues for now

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
 
 function submitIssue(e) {
-  const getInputValue = id => document.getElementById(id).value;
+  const getInputValue = id => document.getElementById(id).value.trim();
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
@@ -15,9 +15,9 @@ function submitIssue(e) {
   }
   issues.push(issue);
   localStorage.setItem('issues', JSON.stringify(issues));
-
   document.getElementById('issueInputForm').reset();
   fetchIssues();
+  issueCounter()
   e.preventDefault();
 }
 
@@ -27,12 +27,30 @@ const closeIssue = id => {
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
+  issueCounter()
 }
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter(issue.id !== id)
+  const remainingIssues = issues.filter((issue) => issue.id !== id)
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues()
+  issueCounter()
+}
+const issueCounter = () => {
+  let open = 0
+  let closed = 0
+  const issues = JSON.parse(localStorage.getItem('issues'))
+  issues.forEach(issue => {
+    if (issue.status === 'Closed') {
+      closed += 1
+    } else if (issue.status === 'Open') {
+      open += 1
+    }
+  })
+
+  document.querySelector('.open').textContent = open
+  document.querySelector('.closed').textContent = `(${closed})`
 }
 
 const fetchIssues = () => {
@@ -45,12 +63,12 @@ const fetchIssues = () => {
 
     issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <p><span class="label ${status === "Closed" ? 'label-warning' : 'label-info'}"> ${status} </span></p>
+                              ${status === 'Closed' ? "<h3><del>" : "<h3>"} ${description} ${status === 'Closed' ? "</h3></del>" : "</h3>"}
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              <a href="#" onclick="closeIssue('${id}')" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
                               </div>`;
   }
 }
